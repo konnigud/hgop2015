a50918400a812217eaccc706d8c009827a6000d3
Konni <konnigud@gmail.com>
2015-12-02 13:07:53 +0000

    adding deployment script

---
+++ DeployToProd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Hello, World"
+
+docker login --username=konnigud --password=comen3tary --email=konnigud@gmail.com
+
+docker push konnigud/tictactoe
+
+test=$(docker ps -a -q)
+echo $test
+
+ssh konni@thundera.org <<'ENDSSH'
+
+contID=$(docker ps -q)
+
+[[ -z $contID ]] && echo no process is running || echo Process $(docker kill $contID) is killed
+
+docker pull konnigud/tictactoe
+
+docker run -p 9080:9080 -d -e "NODE_ENV=production" konnigud/tictactoe
+
+ENDSSH
