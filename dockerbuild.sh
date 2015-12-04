9f2a5fb627b4328afc7801e6e40d1086e2105961
Konni <konnigud@gmail.com>
2015-12-04 23:35:46 +0000

    changes in scripts

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+echo Settings
+echo Path is: $PATH
+echo System user is: $USER
+
 echo Cleaning...
 rm -rf ./dist
 
+echo Setting evironment
+npm install
+bower install
 echo Building app
 grunt
-
+gruntStatus=$?
+if [ $gruntStatus -ne 0 ]; then
+	echo "Grunt build failed with status $gruntStatus";
+	exit $gruntStatus
+fi
 cp ./Dockerfile ./dist/
 
 cd dist
