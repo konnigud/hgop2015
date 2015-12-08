c4ea0cd8cb558f01a978e293501c65d25d4f7d44
Konni <konnigud@gmail.com>
2015-12-08 11:57:21 +0000

    fixing DeployToProd script

--- DeployToProd.sh
+++ DeployToProd.sh
@@ -2,20 +2,12 @@
 
 echo "Deploy to prod starting"
 
-#docker login --username=konnigud --password=comen3tary --email=konnigud@gmail.com
+echo "Logging into docker"
+docker login --username=konnigud --password=comen3tary --email=konnigud@gmail.com
 
-#docker push konnigud/tictactoe
+echo "Pushing changes"
+docker push konnigud/tictactoe
 
-ssh konni@thundera.org ~/deploydocker.sh konnigud/tictactoe
+echo "Deploying $1 to thundera on port $2"
+ssh konni@thundera.org ~/deploydocker.sh $1 $2
 
-#ssh konni@thundera.org <<'ENDSSH'
-
-#contID=$(docker ps -q)
-
-#[[ -z $contID ]] && echo no process is running || echo Process $(docker kill $contID) is killed
-
-#docker pull konnigud/tictactoe
-
-#docker run -p 9001:9001 -d -e "NODE_ENV=production" konnigud/tictactoe
-
-#ENDSSH
