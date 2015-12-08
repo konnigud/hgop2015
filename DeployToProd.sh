12cebcc0857b8d897a44a8cbb14a9f71683d0ad0
Konni <konnigud@gmail.com>
2015-12-08 09:38:54 +0000

    færa deployment yfir á server

--- DeployToProd.sh
+++ DeployToProd.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-echo "Hello, World"
+echo "Deploy to prod starting"
 
-docker login --username=konnigud --password=comen3tary --email=konnigud@gmail.com
+#docker login --username=konnigud --password=comen3tary --email=konnigud@gmail.com
 
-docker push konnigud/tictactoe
+#docker push konnigud/tictactoe
 
-test=$(docker ps -a -q)
-echo $test
+ssh konni@thundera.org ~/deploydocker.sh konnigud/tictactoe
 
-ssh konni@thundera.org <<'ENDSSH'
+#ssh konni@thundera.org <<'ENDSSH'
 
-contID=$(docker ps -q)
+#contID=$(docker ps -q)
 
-[[ -z $contID ]] && echo no process is running || echo Process $(docker kill $contID) is killed
+#[[ -z $contID ]] && echo no process is running || echo Process $(docker kill $contID) is killed
 
-docker pull konnigud/tictactoe
+#docker pull konnigud/tictactoe
 
-docker run -p 9001:9001 -d -e "NODE_ENV=production" konnigud/tictactoe
+#docker run -p 9001:9001 -d -e "NODE_ENV=production" konnigud/tictactoe
 
-ENDSSH
+#ENDSSH
